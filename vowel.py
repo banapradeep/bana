de33dce095d0da6e58da02338b2e7e7407d5c202
banapradeep <40797137+banapradeep@users.noreply.github.com>
2018-07-06 10:26:12 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+list=['a','e','i','o','u','A','E','I','O','U']
+a=(raw_input())
+print (a)
+if(a is in list):
+   print("a is vowel")
+elif(a not in list):
+   print("a is cosonent")
+else:
+   print("a is invalid")
